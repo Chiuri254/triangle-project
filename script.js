3966426320fb1d70e9b7cd2406608dbe448f95d2
Chiuri254 <joseshiory@gmail.com>
2018-12-04 16:55:04 +0300

    new

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-function triangleTracker() {
+function triangleTracker(){
 
-    var number1 = parseFloat(document.getElementById("firstnumber").value);
-    var number2 = parseFloat(document.getElementById("secondnumber").value);
-    var number3 = parseFloat(document.getElementById("thirdnumber").value)
-if ( number1==number2 && number2==number3 && number1 == number3 ){
-  alert('Equilateral-All sides are equal.');
-} else if (number1 == number2 || number1 == number3 ||number2 ==number3){
- alert(' Isosceles-Exactly 2 sides are equal.');
-} else if ((number1 + number2) <= number3 || (number1 + number3) <= number2 ||(number2+number3<=number1 )){
- alert('Scalene-No sides are equal.');
-} else {
-  alert('Not a triangel-A triangel has three side of which two are equal.');
-}
-function triangleTracker() {
-  return number3;
-}
-    }
-// var form = document.querySelector('form');
-// var fname = document.getElementById('fname');
-// var lname = document.getElementById('lname');
-// var submit = document.getElementById('submit');
-// var para = document.querySelector('p');
+  var number1 = parseFloat(document.getElementById("firstnumber").value);
+  var number2 = parseFloat(document.getElementById("secondnumber").value);
+  var number3 = parseFloat(document.getElementById("thirdnumber").value)
+  if (number1 == number2 && number2 == number3 && number1 == number3) {
+    alert('Equilateral-All sides are equal.');
+  } else if (number1 == number2 || number1 == number3 || number2 == number3) {
+    alert(' Isosceles-Exactly 2 sides are equal.');
+  } else if ((number1 + number2) <= number3 || (number1 + number3) <= number2 || (number2 + number3 <= number1)) {
+    alert('Not a triangel-A triangel has three side of which two are equal.');
+  } else {
+    alert('Scalene-No sides are equal.');
+  }
 
-// form.onsubmit = function(e) {
-//   if (fname.value === '' || lname.value === '') {
-//     e.preventDefault();
-//     para.textContent = 'You need to fill in both names!';
-//   }
-// }
+  function triangleTracker(){
+    return number3;
+  }
+}
